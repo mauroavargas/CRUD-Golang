714ab469062056dc29d69e5bcfc655f88c67d85a
Mauro Vargas <maurovargas@MacBook-Pro-de-Mauro.local>
2021-08-12 06:19:22 -0500

    Server config file and templates

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
+
+var templates = template.Must(template.ParseGlob("templates/*"))
 
 func main() {
 	http.HandleFunc("/", Index)
 	log.Println("Server running!")
+	http.ListenAndServe(":8080", nil)
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
