e2ba3772a98422e20ec6fe0c267b40cf3dea8221
Mauro Vargas <maurovargas@MacBook-Pro-de-Mauro.local>
2021-10-11 05:13:58 -0500

    Add functions to edit, delete and update

--- main.go
+++ main.go
@@ -29,6 +29,10 @@ func main() {
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/add", Add)
 	http.HandleFunc("/insert", Insert)
+	http.HandleFunc("/delete", Delete)
+	http.HandleFunc("/edit", Edit)
+	http.HandleFunc("/update", Update)
+
 	fmt.Println("Server running!")
 	http.ListenAndServe(":8080", nil)
 }
@@ -91,6 +95,72 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 			panic(err.Error())
 		}
 		addRecords.Exec(name, email)
+
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+
+	}
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	employeeId := r.URL.Query().Get("id")
+	fmt.Println(employeeId)
+
+	establishedConnection := connectDB()
+	deleteRecords, err := establishedConnection.Prepare("DELETE FROM employees WHERE id=?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deleteRecords.Exec(employeeId)
+
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	employeeId := r.URL.Query().Get("id")
+
+	establishedConnection := connectDB()
+	getRecords, err := establishedConnection.Query("SELECT * FROM employees WHERE id=?", employeeId)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	employee := Employee{}
+
+	for getRecords.Next() {
+		var id int
+		var name, email string
+		err = getRecords.Scan(&id, &name, &email)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		employee.Id = id
+		employee.Name = name
+		employee.Email = email
+	}
+
+	templates.ExecuteTemplate(w, "edit", employee)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+
+		id := r.FormValue("employeeId")
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+
+		establishedConnection := connectDB()
+		updateRecords, err := establishedConnection.Prepare("UPDATE employees SET name=?,email=? WHERE id=?")
+
+		if err != nil {
+			panic(err.Error())
+		}
+		updateRecords.Exec(name, email, id)
 
 		http.Redirect(w, r, "/", http.StatusMovedPermanently)
 
